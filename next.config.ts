40346336f4553788bf8f1f3989cb73699643ecf6
Elyor <elyorabdufattokhov@gmail.com>
2025-10-04 23:35:42 +0500

    bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config: Configuration) {
+    if (config.resolve && typeof config.resolve.alias === "object") {
+      (config.resolve.alias as Record<string, string>)["@"] = path.resolve(__dirname, "src");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
